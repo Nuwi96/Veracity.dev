f17416a71fc41cbb2262272e07644e18849d3011
Nuwi96 <92671065+Nuwi96@users.noreply.github.com>
2021-11-14 23:10:36 +0530

    remove un wanted codes

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -5,19 +5,19 @@ const search = (text: string) => {
 };
 
 const getMovies = () => {
-    return http.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=66ed895e7cd5320bdb1a8245e2a25f6a`);
+    return http.get(`discover/movie?sort_by=popularity.desc&api_key=66ed895e7cd5320bdb1a8245e2a25f6a`);
 };
 
 const getGenreList = () => {
-    return http.get('https://api.themoviedb.org/3/genre/movie/list?api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
+    return http.get('genre/movie/list?api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
 }
 
 const getSelectedMovie = (id: string) => {
-    return http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
+    return http.get('movie/' + id + '?api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
 }
 
 const filterData = (genre: any, rating: any, sortBy: any, year: any) => {
-    return http.get('https://api.themoviedb.org/3/discover/movie?with_genres=' + genre + '&primary_release_year=' + year + '&sort_by=' + sortBy + '&vote_average.gte=' + rating + '&api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
+    return http.get('discover/movie?with_genres=' + genre + '&primary_release_year=' + year + '&sort_by=' + sortBy + '&vote_average.gte=' + rating + '&api_key=66ed895e7cd5320bdb1a8245e2a25f6a')
 }
 
 const MovieService = {
